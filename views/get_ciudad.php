0c0c1205f262ef68be025d9ecb0f50e8ce41558f
luluisyo <luluispuro@gmail.com>
2019-05-15 15:46:17 -0400

    mejoramiento de formulario

--- views/get_ciudad.php
+++ views/get_ciudad.php
@@ -4,7 +4,7 @@
     if(!empty($_POST["id_pais"])) 
     {
          $conn = odbc_connect("Driver=FreeTDS;DSN=test;Database=MOLINO_ANDINO", 'Reportes', 'Bolivia12345');
-          $query = "SELECT ItemCode, ItemName, ItmsGrpCod ,InvntItem FROM OITM WHERE ItmsGrpCod = 108";
+          $query = "SELECT ItemCode, ItemName, ItmsGrpCod ,InvntItem FROM OITM WHERE ItmsGrpCod = '" . $_POST["id_pais"] . "'";
 print $query;
-}    
+    }
 ?>
