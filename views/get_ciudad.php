8bcd2ed2b61bc72e92a316b6ef6469a9d5bbb94e
luluisyo <luluispuro@gmail.com>
2019-05-15 15:48:56 -0400

    formulario fila uno

--- views/get_ciudad.php
+++ views/get_ciudad.php
@@ -6,5 +6,20 @@
          $conn = odbc_connect("Driver=FreeTDS;DSN=test;Database=MOLINO_ANDINO", 'Reportes', 'Bolivia12345');
           $query = "SELECT ItemCode, ItemName, ItmsGrpCod ,InvntItem FROM OITM WHERE ItmsGrpCod = '" . $_POST["id_pais"] . "'";
 print $query;
+          $result = odbc_exec($conn, $query);
+       
+       //construimos lista nueva dependiente
+      ?>
+      <option value="">Seleccionar Ciudad</option>
+      <?php
+ 
+              while(odbc_fetch_row($result)){
+          $idgrupo = odbc_result($result, 1);$desgrupo = odbc_result($result, 2);
+	   ?>
+	      <option value="<?php echo $idgrupo; ?>" data-value="<?php echo $desgrupo; ?>"></option>
+	   <?php
+      }
     }
+       odbc_close($conn);
+
 ?>
