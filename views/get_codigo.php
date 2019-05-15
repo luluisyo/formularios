7d9fc8ae04c6fc6adfc9480c149e48774ce097ea
luluisyo <luluispuro@gmail.com>
2019-05-15 16:17:07 -0400

    formulario fila uno

--- views/get_codigo.php
+++ views/get_codigo.php
@@ -7,6 +7,7 @@
                $conn = odbc_connect("Driver=FreeTDS;DSN=test;Database=MOLINO_ANDINO", 'Reportes', 'Bolivia12345');
           $query = "SELECT ItemCode, ItemName, ItmsGrpCod ,InvntItem FROM OITM WHERE ItemCode= '" . $_GET["codigo"] . "'";
               
+              
               $result = odbc_exec($conn, $query);
    
               while(odbc_fetch_row($result)){
