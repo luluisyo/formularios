dfccf6192f8f3342caa40942a71c4b415cf404ba
luluisyo <luluispuro@gmail.com>
2019-05-15 15:45:28 -0400

    mejoramiento de formulario

--- views/get_ciudad.php
+++ views/get_ciudad.php
@@ -6,5 +6,5 @@
          $conn = odbc_connect("Driver=FreeTDS;DSN=test;Database=MOLINO_ANDINO", 'Reportes', 'Bolivia12345');
           $query = "SELECT ItemCode, ItemName, ItmsGrpCod ,InvntItem FROM OITM WHERE ItmsGrpCod = 108";
 print $query;
-    
+}    
 ?>
