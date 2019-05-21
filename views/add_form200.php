f4999bb4131d67fbff18c1a7ac62f3c79967f1cf
luluisyo <luluispuro@gmail.com>
2019-05-21 16:34:28 -0400

    proformas

--- views/add_form200.php
+++ views/add_form200.php
@@ -346,7 +346,7 @@ function obtenerCiudades10()
   m2 = document.getElementById("precio1").value;
   r = m1*m2;
   document.getElementById("total1").value = r;
- document.getElementById("totalt").value = document.getElementById("totalt").value + r;
+ document.getElementById("totalt").value = parseInt(document.getElementById("totalt").value) + r;
 
 }
 
